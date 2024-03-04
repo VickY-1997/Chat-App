2e02bd9b193e8a935f3c8ba8641a34d4847028f2
VickY-1997 <vigneshkumar3497@gmail.com>
2024-03-04 18:10:12 +0530

    socket url updated

--- frontEnd/src/context/socketContext.jsx
+++ frontEnd/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children}) => {
 
     useEffect( () => {
         if(authUser){
-            const socket = io('http://localhost:4545', {
+            const socket = io("https://chat-app-7wri.onrender.com", {
                 query:{
                     userId: authUser._id
 
